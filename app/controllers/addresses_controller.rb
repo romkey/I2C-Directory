73110549c21400b96c2faa3375aea43347a86be7
John Romkey <58883+romkey@users.noreply.github.com>
2018-08-13 13:18:45 -0700

    work around weird rails error on Heroku

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -64,7 +64,7 @@ class AddressesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_address
-      @address = Address.find_by_slug(params[:slug])
+      @address = Address.find_by slug: params[:slug]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
