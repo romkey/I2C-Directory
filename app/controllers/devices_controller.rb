2d607812bd19164607b28b5d57d349a0b0800852
John Romkey <58883+romkey@users.noreply.github.com>
2018-08-12 21:11:59 -0700

    added manufacturer to permitted device params
    bootstrapified device edit form

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -73,6 +73,6 @@ class DevicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def device_params
-      params.require(:device).permit(:part_number, :friendly_name, :addresses, :reserved, :obsolete, :datasheet, :adafruit, :sparkfun, :amazon)
+      params.require(:device).permit(:part_number, :friendly_name, :addresses, :manufacturer, :suggestion, :obsolete, :datasheet, :adafruit, :sparkfun, :amazon)
     end
 end
