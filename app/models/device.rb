ce6253b194210c9ae92114993f1b1c030c814d6f
John Romkey <58883+romkey@users.noreply.github.com>
2018-10-08 06:56:21 -0700

    omit views and scanned attributes on device download

--- app/models/device.rb
+++ app/models/device.rb
@@ -23,7 +23,7 @@ class Device < ApplicationRecord
   end
 
   def as_json(**args)
-    args[:except] = [ "id", "slug", "attribution", "created_at", "updated_at", "suggestion" ]
+    args[:except] = [ "id", "slug", "attribution", "created_at", "updated_at", "suggestion", "views", "scanned" ]
     result = self.old_as_json(args)
 
     result.delete_if { |key, value| value.nil? || value == ''}
