80c10b3ac89be7263b8cacf2b364ef400a7d3dba
John Romkey <58883+romkey@users.noreply.github.com>
2018-10-08 06:49:57 -0700

    allow new attributesd to be null

--- db/migrate/20181006054030_add_fields_to_device.rb
+++ db/migrate/20181006054030_add_fields_to_device.rb
@@ -1,13 +1,13 @@
 class AddFieldsToDevice < ActiveRecord::Migration[5.2]
   def change
     add_column :devices, :release_date, :datetime
-    add_column :devices, :is_5v, :boolean, null: false, default: false
-    add_column :devices, :is_3v, :boolean, null: false, default: false
-    add_column :devices, :is_standard_speed, :boolean, null: false, default: false
-    add_column :devices, :is_full_speed, :boolean, null: false, default: false
-    add_column :devices, :is_fast_speed, :boolean, null: false, default: false
-    add_column :devices, :is_high_speed, :boolean, null: false, default: false
-    add_column :devices, :is_ultra_fast_speed, :boolean, null: false, default: false
-    add_column :devices, :is_spi, :boolean, null: false, default: false
+    add_column :devices, :is_5v, :boolean
+    add_column :devices, :is_3v, :boolean
+    add_column :devices, :is_standard_speed, :boolean
+    add_column :devices, :is_full_speed, :boolean
+    add_column :devices, :is_fast_speed, :boolean
+    add_column :devices, :is_high_speed, :boolean
+    add_column :devices, :is_ultra_fast_speed, :boolean
+    add_column :devices, :is_spi, :boolean
   end
 end
