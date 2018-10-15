ff24f3ee900515cae3cbec541c29e85ea81e123c
John Romkey <58883+romkey@users.noreply.github.com>
2018-10-14 21:37:00 -0700

    fix migration

--- db/migrate/20181014222632_add_drivers_to_devices.rb
+++ db/migrate/20181014222632_add_drivers_to_devices.rb
@@ -1,6 +1,6 @@
 class AddDriversToDevices < ActiveRecord::Migration[5.2]
   def change
-    add_column :devices, :drivers, :jsonb, null: false, default: '[]'
-    remove_column :devices, :library
+    add_column :devices, :drivers, :jsonb, null: false, default: []
+    remove_column :devices, :library, :string
   end
 end
